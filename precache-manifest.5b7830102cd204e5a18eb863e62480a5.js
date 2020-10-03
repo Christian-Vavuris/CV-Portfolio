bd0cfce182fc72a653de4208f7b2776284a71dba
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-10-03 11:46:20 -0700

    Updates

---
+++ precache-manifest.5b7830102cd204e5a18eb863e62480a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012c8e882959a24c40dae9dfe892ab19",
+    "url": "/CV-Portfolio/index.html"
+  },
+  {
+    "revision": "76a8898a49da8832d4cf",
+    "url": "/CV-Portfolio/static/css/main.3176e1e5.chunk.css"
+  },
+  {
+    "revision": "af4cbe83bd2773b6f844",
+    "url": "/CV-Portfolio/static/js/2.8cc2af36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV-Portfolio/static/js/2.8cc2af36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a8898a49da8832d4cf",
+    "url": "/CV-Portfolio/static/js/main.7cf04e35.chunk.js"
+  },
+  {
+    "revision": "fc3dbf7ad93d2958e33c",
+    "url": "/CV-Portfolio/static/js/runtime-main.5d80acd0.js"
+  },
+  {
+    "revision": "b029fa874766c9c39fdb96348df8fa29",
+    "url": "/CV-Portfolio/static/media/Headshot.b029fa87.jpg"
+  },
+  {
+    "revision": "ef5c99ee34f180f012667089f3ff9d06",
+    "url": "/CV-Portfolio/static/media/cover-image.ef5c99ee.jpg"
+  }
+]);
